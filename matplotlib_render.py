13a34728211014338b436d3c2f01ce7760ea8d25
chacorp <sihun.cha@gmail.com>
2024-05-21 15:36:39 +0000

    gouraud shading

--- matplotlib_render.py
+++ matplotlib_render.py
@@ -88,14 +88,53 @@ def calc_norm_fv(fv):
     norm = norm / (np.linalg.norm(norm, axis=-1)[ :, np.newaxis] + 1e-12)
     return norm
     
-def plot_image_array(Vs, Fs, rot_list=None, size=6, norm=False, mode='mesh', linewidth=1, linestyles='solid'): 
+def plot_image_array(Vs, 
+                     Fs, 
+                     rot_list=None, 
+                     size=6, 
+                     norm=False, 
+                     view_mode='p'
+                     mode='mesh', 
+                     linewidth=1, 
+                     linestyle='solid', 
+                     light_dir=np.array([0,0,1]),
+                     bg_black = True,
+                     logdir='.', 
+                     name='000', 
+                     save=False,
+                    ):
+    r"""
+    Args:
+        Vs (list): list of vertices [V, V, V, ...]
+        Fs (list): list of face indices [F, F, F, ...]
+        rot_list (list): list of euler angle [ [x,y,z], [x,y,z], ...]
+        size (int): size of figure
+        norm (bool): if True, normalize vertices
+        view_mode (str): if 'p' use perspective, if 'o' use orthogonal camera
+        mode (str): mode for rendering [mesh(wireframe), shade, normal]
+        linewidth (float): line width for wireframe (kwargs for matplotlib)
+        linestyle (str): line style for wireframe (kwargs for matplotlib)
+        light_dir (np.array): light direction
+        bg_black (bool): if True, use dark_background for plt.style
+        logdir (str): directory for saved image
+        name (str): name for saved image
+        save (bool): if True, save the plot as image
+    """
+    if mode=='gouraud':
+        print("currently WIP!: need to curl by z")
+        
     num_meshes = len(Vs)
+    if bg_black:
+        plt.style.use('dark_background')
+    else:
+        plt.style.use('default')
+    
     fig = plt.figure(figsize=(size * num_meshes, size))  # Adjust figure size based on the number of meshes
     
     for idx, (V, F) in enumerate(zip(Vs, Fs)):
         # Calculate the position of the subplot for the current mesh
         ax_pos = [idx / num_meshes, 0, 1 / num_meshes, 1]
-        ax = fig.add_axes(ax_pos, xlim=[-1, +1], ylim=[-1, +1], aspect=1, frameon=True)
+        ax = fig.add_axes(ax_pos, xlim=[-1, +1], ylim=[-1, +1], aspect=1, frameon=False)
 
         #xrot, yrot, zrot = rot[0], 90, rot[2]
         if rot_list:
@@ -103,10 +142,14 @@ def plot_image_array(Vs, Fs, rot_list=None, size=6, norm=False, mode='mesh', lin
         else:
             xrot, yrot, zrot = 0,0,0
         ## MVP
-        model = translate(0, 0, -6) @ yrotate(yrot) @ xrotate(xrot) @ zrotate(zrot)
-        #proj  = perspective(30, 1, 1, 100)
-        proj  = ortho(-1, 1, -1, 1, 1, 100) # Use ortho instead of perspective
+        model = translate(0, 0, -4) @ yrotate(yrot) @ xrotate(xrot) @ zrotate(zrot)
+        if view_mode=='p':
+            proj  = perspective(55, 1, 1, 100)
+        else:
+            proj  = ortho(-1, 1, -1, 1, 1, 100) # Use ortho instead of perspective
+            
         MVP   = proj @ model # view is identity
+        
         # quad to triangle    
         VF_tri = transform_vertices(V, MVP, F, norm)
 
@@ -116,35 +159,70 @@ def plot_image_array(Vs, Fs, rot_list=None, size=6, norm=False, mode='mesh', lin
         Z = (Z - zmin) / (zmax - zmin)
 
         if mode=='normal':
-            C = calc_norm_fv(V[F]) @ model[:3,:3].T
+            C = calc_face_norm(V[F]) @ model[:3,:3].T
             
             I = np.argsort(Z)
             T, C = T[I, :], C[I, :]
 
             NI = np.argwhere(C[:,2] > 0).squeeze()
             T, C = T[NI, :], C[NI, :]
-            C = np.clip(C, 0, 1) if False else C * 0.5+ 0.5
-            collection = PolyCollection(T, closed=False, linewidth=linewidth, facecolor=C, edgecolor=C, linestyles=linestyles)
+            C = np.clip(C, 0, 1) if False else C * 0.5 + 0.5
+            collection = PolyCollection(T, closed=False, linewidth=linewidth, facecolor=C, edgecolor=C)
         elif mode=='shade':
-            C = calc_norm_fv(V[F]) @ model[:3,:3].T
+            C = calc_face_norm(V[F]) @ model[:3,:3].T
             
             I = np.argsort(Z)
             T, C = T[I, :], C[I, :]
 
             NI = np.argwhere(C[:,2] > 0).squeeze()
             T, C = T[NI, :], C[NI, :]
-            C = (C @ np.array([0,0,1]))[:,np.newaxis].repeat(3,1)
-            print(C.shape)
+            
+            C = (C @ light_dir)[:,np.newaxis].repeat(3, axis=-1)
             C = np.clip(C, 0, 1)
-            collection = PolyCollection(T, closed=False, linewidth=linewidth, facecolor=C, edgecolor=C, linestyles=linestyles)
+            C = C*0.5+0.25
+            collection = PolyCollection(T, closed=False, linewidth=linewidth,facecolor=C, edgecolor=C)
+        elif mode=='gouraud':
+#             I = np.argsort(Z)
+#             V, F, vidx = get_new_mesh(V, F, I, invert=True)
+            
+            ### curling by normal
+            C = calc_norm(V, F, mode='v') #@ model[:3,:3].T
+            NI = np.argwhere(C[:,2] > 0.0).squeeze()
+            V, F, vidx = get_new_mesh(V, F, NI, invert=True)
+            
+            C = calc_norm(V, F,mode='v') #@ model[:3,:3].T
+            
+            #VV = (V-V.min()) / (V.max()-V.min())# world coordinate
+            V = transform_vertices(V, MVP, F, norm, no_parsing=True)
+            triangle_ = tri.Triangulation(V[:,0], V[:,1], triangles=F)
+            
+            C = (C @ light_dir)[:,np.newaxis].repeat(3, axis=-1)
+            C = np.clip(C, 0, 1)
+            C = C*0.5+0.25
+            #VV = (V-V.min()) / (V.max()-V.min()) #screen coordinate
+            #cmap = colors_to_cmap(VV)
+            cmap = colors_to_cmap(C)
+            zs = np.linspace(0.0, 1.0, num=V.shape[0])
+            plt.tripcolor(triangle_, zs, cmap=cmap, shading='gouraud')
+            
         else:
             C = plt.get_cmap("gray")(Z)
             I = np.argsort(Z)
             T, C = T[I, :], C[I, :]
-            collection = PolyCollection(T, closed=False, linewidth=linewidth, facecolor=C, edgecolor='black')
-        
-        ax.add_collection(collection)
-    plt.show()
+            
+            collection = PolyCollection(T, closed=False, linewidth=0.23, facecolor=C, edgecolor='black')
+            
+        if mode!='gouraud':
+            ax.add_collection(collection)
+        plt.xticks([])
+        plt.yticks([])
+    
+    if save:
+        plt.savefig('{}/{}.png'.format(logdir, name), bbox_inches = 'tight')
+        plt.close()
+    else:
+        plt.show()
+        plt.close()
     
 def render_video(basedir="tmp", # mesh obj directory
                  savedir="tmp", # save path 
